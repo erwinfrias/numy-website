ed9da5723f87b7fc22c8890dddfe5cac1fd29485
Erwin Alan Frías Martínez <alan.frias@erwcode.com>
2021-07-19 15:32:06 -0500

    menu component :sushi:

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import favicon from './images/favicon.png'
 import logo from './images/logo.svg';
 import logoWhite from './images/logo-white.svg';
 
+import iconLanguage from './images/icon-language.svg'
 
 
 /*  CSS
@@ -14,3 +15,7 @@ import scss from './scss/main.scss'
 
 /*  JavaScript
 \*-------------------------------------------------------*/
+import { activeMenu, activeMenuLink } from './js/menu'
+
+activeMenu('hamburger','menu')
+activeMenuLink()
