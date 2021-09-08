f84ee495567aa1a607a76e84fee706fe2d7fe669
Erwin Alan Frías Martínez <alan.frias@erwcode.com>
2021-09-07 20:08:37 -0500

    print new exclusive listings :satellite::sparkles:

--- src/js/bridge_API.js
+++ src/js/bridge_API.js
@@ -1,27 +1,33 @@
 const DATASET_ID = process.env.DATASET_ID
 const SERVER_TOKEN = process.env.SERVER_TOKEN
+const API_URL = `https://api.bridgedataoutput.com/api/v2/OData/${DATASET_ID}/Property?access_token=${SERVER_TOKEN}`
 
-const _projects = document.getElementById('projects')
-const _projectsTemplate = document.getElementById('projectsTemplate')
+const _projects = 'projects'
+const _projectsTemplate = 'projectsTemplate'
+
+const _exclusive = 'exclusive'
+const _exclusiveTemplate = 'exclusiveTemplate'
 
 export const bridgeData = async () => {
   try {
-    const res = await  fetch(`https://api.bridgedataoutput.com/api/v2/OData/${DATASET_ID}/Property?access_token=${SERVER_TOKEN}`)
-    const data = await res.json()
-
     // Get last 3 projects
-    const restProjects = await fetch(`https://api.bridgedataoutput.com/api/v2/OData/${DATASET_ID}/Property?access_token=${SERVER_TOKEN}&$filter=((NewConstructionYN ne false))&$top=3`)
-    const dataProjects = await restProjects.json()
+    const restLastProjects = await fetch(`${API_URL}&$filter=((NewConstructionYN ne false))&$top=3`)
+    const dataLastProjects = await restLastProjects.json()
 
-    printProperty(dataProjects, _projects, _projectsTemplate)
+    // Get last 6 exclusive listings
+    const restLastExclusive = await fetch(`${API_URL}&$filter=ListPrice gt 1500000&$top=6`)
+    const dataLastExclusive = await restLastExclusive.json()
+
+    printProperty(dataLastProjects, _projects, _projectsTemplate)
+    printListingProperty(dataLastExclusive, _exclusive, _exclusiveTemplate)
 
   } catch (error) {
     console.log(console.error)
   }
 }
 
-const printProperty = (data, container, templateProperty) => {
-  const template = templateProperty.content
+const printProperty = (data, containerID, templateID) => {
+  const template = document.getElementById(templateID).content
   const fragment = new DocumentFragment()
 
   data.value.forEach(property => {
@@ -34,5 +40,24 @@ const printProperty = (data, container, templateProperty) => {
     fragment.appendChild(clone)
   })
 
-  container.appendChild(fragment)
+  document.getElementById(containerID).appendChild(fragment)
+}
+
+const printListingProperty = (data, containerID, templateID) => {
+  const template = document.getElementById(templateID).content
+  const fragment = new DocumentFragment()
+
+  data.value.forEach(property => {
+    template.querySelector('img').setAttribute('src', property.Media[0].MediaURL)
+    template.querySelector('img').setAttribute('alt', property.BuildingName)
+    template.querySelector('.card__price').textContent = property.ListPrice
+    template.querySelector('p').textContent = `${property.BedroomsTotal} Bed(s) ${property.BathroomsTotalDecimal} Bath(s) Sq.Ft ${property.LotSizeSquareFeet}`
+    template.querySelector('address').textContent = property.UnparsedAddress
+    template.querySelector('a').dataset.id = property.ListingId
+
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+
+  document.getElementById(containerID).appendChild(fragment)
 }
